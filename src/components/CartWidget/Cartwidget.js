b35ca455a9a9ca5295d94a009eb18c3fff655bdb
Lucas Yacusa <lyacusa99@gmail.com>
2022-07-23 13:36:49 -0300

    Mejora en el responsive del navbar

--- src/components/CartWidget/Cartwidget.js
+++ src/components/CartWidget/Cartwidget.js
@@ -3,7 +3,7 @@ import './Cartwidget.css'
 const CartWidget = () => {
     return (
         <div className="carwid">
-            <img src='images/carrito.png' alt='CartWidget'/>
+            <img src='images/navbar/carrito2.png' alt='CartWidget'/>
             <p>5</p>
         </div>
     )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,13 @@ import CartWidget from '../CartWidget/Cartwidget'
 const Navbar = () => {
     return (
         <nav>
-            <div>
+            <div className="contmenuburger">
+                <img src='images/navbar/menuburger2.png' alt='CartWidget'/>
+            </div>
+            <div className="contnamepage">
                 <h1 className="namepage">Todo3D</h1>
             </div>
-            <div>
+            <div className="contbutt">
                 <button>Chucherias</button>
                 <button>Coleccionables</button>
                 <button>Pesca</button>
