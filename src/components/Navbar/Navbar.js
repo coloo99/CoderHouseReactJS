07ab0d997520749c163248a31cb47eef6d5cb9d8
Lucas Yacusa <lyacusa99@gmail.com>
2022-07-23 15:10:51 -0300

    Menu burger quedó funcionando correctamente

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,18 @@
+import React, { useState } from "react";
 import './Navbar.css'
 import CartWidget from '../CartWidget/Cartwidget'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <nav>
-            <div className="contmenuburger">
+            <div className={`contmenuburger ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
                 <img src='images/navbar/menuburger2.png' alt='CartWidget'/>
             </div>
             <div className="contnamepage">
                 <h1 className="namepage">Todo3D</h1>
             </div>
-            <div className="contbutt">
+            <div className={`contbutt ${isOpen && "open"}`}>
                 <button>Chucherias</button>
                 <button>Coleccionables</button>
                 <button>Pesca</button>
