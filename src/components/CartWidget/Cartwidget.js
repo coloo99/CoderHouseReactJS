a7ec8713080e56f54366a1a1a948e4a1b1a0c2de
Lucas Yacusa <lyacusa99@gmail.com>
2022-08-10 20:37:29 -0300

    Se crea cart context con sus respectivas funciones para darle vida al carrito

--- src/components/CartWidget/Cartwidget.js
+++ src/components/CartWidget/Cartwidget.js
@@ -1,10 +1,17 @@
 import './Cartwidget.css'
+import { useContext } from 'react'
+import CartContext from '../../context/CartContext'
 
 const CartWidget = () => {
+
+    const { getQuantity } = useContext(CartContext)
+
+    const quantity = getQuantity()
+
     return (
         <div className="carwid">
             <img className='cart' src={`${process.env.PUBLIC_URL}/images/navbar/carrito2.png`} alt='CartWidget'/>
-            <p className='countCart'>5</p>
+            {<p className='countCart'>{quantity}</p>}
         </div>
     )
 }
